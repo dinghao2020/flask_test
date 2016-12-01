fcc6e23959eddbb41936b8668e8d6bf93e42db32
dinghao <18001326539@163.com>
2016-12-01 17:11:45 +0800

    flash test

---
+++ flask/f2.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+UPLOAD_FOLDER = '/home/dh/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
